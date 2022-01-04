5be7170324c9a8b5ac177487f8d14de7abf13410
Alex J. Lee <alexjlee.dev@gmail.com>
2022-01-04 11:43:37 +0900

    Design: Home 페이지 UI

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,18 +1,52 @@
-import styled from "styled-components";
-import { color } from '../styles'
+import styled from 'styled-components';
+import { color, radius } from '../styles';
 
 const ButtonTemplate = styled.button`
-    width: ${props => props.width};
-    height: ${props => props.height};
-    border-radius: 20px;
-    background-color: ${props => color[props.color]};
-    cursor: pointer;
-`
+  position: relative;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  padding: 0 1rem;
+  border-radius: ${radius};
+  background-color: ${(props) => color[props.color]};
+  cursor: pointer;
+  overflow: hidden;
 
-const Button = ({ width="100%", height="40px", color="secondary", content="BUTTON", handler= () => {} }) => {
-    return (
-        <ButtonTemplate width={width} height={height} color={color} onClick={handler}>{content}</ButtonTemplate>
-    )
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: black;
+    opacity: 0;
+    transition: all 0.2s ease;
+  }
+
+  &:hover {
+    &::after {
+      opacity: 0.1;
+    }
+  }
+`;
+
+const Button = ({
+  width = '100%',
+  height = '40px',
+  color = 'secondary',
+  content = 'BUTTON',
+  handler = () => {},
+}) => {
+  return (
+    <ButtonTemplate
+      width={width}
+      height={height}
+      color={color}
+      onClick={handler}
+    >
+      {content}
+    </ButtonTemplate>
+  );
 };
 
 export default Button;
