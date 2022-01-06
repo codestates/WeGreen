ac6c2fec32249609dbfc2f5b2066dfd03cf6fc66
Alex J. Lee <alexjlee.dev@gmail.com>
2022-01-06 09:22:30 +0900

    Feat: 로그인 기능 구현

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 80;
-const router = require("./routes");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/", router);
 app.use(cookieParser());
 
 app.use(
   cors({
     origin: true,
     credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
+    methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello WeGreen!");
+app.use('/', router);
+
+app.get('/', (req, res) => {
+  res.send('Hello WeGreen!');
 });
 
 app.listen(port, () => {
