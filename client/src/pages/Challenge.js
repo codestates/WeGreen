2edbac6189afcc3731d56cb8916a98b034757fd3
Alex J. Lee <alexjlee.dev@gmail.com>
2022-01-11 09:06:13 +0900

    Feat: 챌린지 페이지 화면크기에 따른 컴포넌트 랜더링

--- client/src/pages/Challenge.js
+++ client/src/pages/Challenge.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { color } from '../styles';
@@ -15,6 +15,42 @@ const ChallengeContainer = styled.div`
 const Challenge = () => {
   const [view, setView] = useState('info');
 
+  const getWindowWidth = () => {
+    const { innerWidth: width } = window;
+    return width;
+  };
+
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+  const throttle = (func, waits) => {
+    let lastFunc; // timer id of last invocation
+    let lastRan; // time stamp of last invocation
+    return function (...args) {
+      const context = this;
+      if (!lastRan) {
+        func.apply(context, args);
+        lastRan = Date.now();
+      } else {
+        clearTimeout(lastFunc);
+        lastFunc = setTimeout(() => {
+          if (Date.now() - lastRan >= waits) {
+            func.apply(context, args);
+            lastRan = Date.now();
+          }
+        }, waits - (Date.now() - lastRan));
+      }
+    };
+  };
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(getWindowWidth());
+    }
+    window.addEventListener('resize', throttle(handleResize, 500));
+    return () =>
+      window.removeEventListener('resize', throttle(handleResize, 500));
+  }, []);
+
   const tabContent = {
     info: <ChallengeInfo></ChallengeInfo>,
     checkin: <ChallengeCheckin></ChallengeCheckin>,
@@ -24,15 +60,25 @@ const Challenge = () => {
     <ChallengeContainer>
       Dummy Challenge {useParams().id}
       <Button href='/editchallenge' />
-      <Tab
-        tabInfo={[
-          ['info', '정보'],
-          ['checkin', '체크인'],
-          ['comments', '댓글'],
-        ]}
-        handleView={setView}
-      />
-      {tabContent[view]}
+      {windowWidth < 1024 ? (
+        <Tab
+          tabInfo={[
+            ['info', '정보'],
+            ['checkin', '체크인'],
+            ['comments', '댓글'],
+          ]}
+          handleView={setView}
+        />
+      ) : null}
+      {windowWidth < 1024 ? (
+        tabContent[view]
+      ) : (
+        <>
+          <ChallengeInfo />
+          <ChallengeCheckin />
+          <ChallengeComments />
+        </>
+      )}
     </ChallengeContainer>
   );
 };
