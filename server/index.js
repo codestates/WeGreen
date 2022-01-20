2fa67a9b0e3452284cc923ca805808a65d6cd027
BoRa Yoon <89366567+boraborayoon@users.noreply.github.com>
2022-01-20 16:12:18 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", ['https://wegreen.link']);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", ['https://wegreen.link']);
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(
   cors({
